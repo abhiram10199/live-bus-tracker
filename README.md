1af22f5bf9eeced34823cc7dc6ca94955814ff49
Lemar Tokham <ltokham@gmail.com>
2025-05-11 22:46:56 +0100

    Initial commit: Real time bus tracker

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bus tracking web app (Flask + WebSockets + Leaflet)
+
+A real-time web app that fetches live bus data from the liverpool area (functionality to add custom busses still to come!).
+
+## Setup
+```bash
+git clone https://github.com/YOUR_USERNAME/YOUR_REPO.git
+cd YOUR_REPO
+python -m venv env
+source env/bin/activate  # or .\env\Scripts\activate on Windows
+pip install -r requirements.txt
+python app.py
+```
+To open, visit http://localhost:3000 on your machine
+
---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, request, jsonify, render_template
+from flask_sock import Sock
+from flask_cors import CORS
+import requests
+import xml.etree.ElementTree as ET
+import threading
+from flask_socketio import SocketIO, send, emit
+
+# Setup
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+CORS(app)
+socketio = SocketIO(app)
+
+# Stores locations from api calls
+locations = []
+
+# Fetching data
+def fetch_data():
+    global locations
+    api_url = "https://data.bus-data.dft.gov.uk/api/v1/datafeed/708/?api_key=ea08777eb34f28317f57a89bebcd105381ba3f96"
+
+    response = requests.get(api_url)
+    print("Hi")
+
+    # Writing to the file
+    with open('location.xml', 'wb') as f:
+        f.write(response.content)
+
+    # Parsing XML file
+    tree = ET.parse('location.xml')
+
+    # Getting the tree structure
+    root = tree.getroot()
+
+    # Dictionary mapping for namespace
+    ns = {'siri': 'http://www.siri.org.uk/siri'}
+
+    # Clear list before populating
+    locations = []
+
+    # Get all the 699 bus
+    for line in root.findall(".//siri:VehicleActivity/siri:MonitoredVehicleJourney/[siri:LineRef='13']",ns):
+        # get their locations
+        long = line.find('siri:VehicleLocation/siri:Longitude', ns)
+        lat = line.find('siri:VehicleLocation/siri:Latitude', ns)
+        # currently stores the location of the very last bus
+        locations.append({'long': long.text, "lat":lat.text})
+
+    # Send to frontend
+    socketio.emit('bus locations', locations)
+
+    print(locations)
+
+    threading.Timer(10, fetch_data).start()
+fetch_data()
+
+
+# Flask endpoints
+
+@app.get("/locations")
+def get_locations():
+    return jsonify(locations)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+    
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=3000)
